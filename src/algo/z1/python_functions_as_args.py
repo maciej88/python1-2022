26ba7b720e77f7fb2bf83ae369fb83b9e4df52e7
maciej <mkrywult88@gmail.com>
2023-03-05 21:36:55 +0100

    solution for exercise

--- src/algo/z1/python_functions_as_args.py
+++ src/algo/z1/python_functions_as_args.py
@@ -5,16 +5,36 @@ def sqare(x):
 def cube(x):
     return x ** 3
 
+def add(a,b):
+    return a+b
+
+def mul(a, b):
+    return a*b
+
+# easy way:
+# def calculate(f, a, b):
+#     if f == add:
+#         return a+b
+#     if f == mul:
+#         return a*b
+#     else:
+#         print('wrong shortcut definition')
+
+def calculate(f, a, b=0):
+    try:
+        return f(a, b)
+    except TypeError:
+        return f(a)
 
 def calculator(operation, argument):
     return operation(argument)
 
-
-print(sqare(4))
-print(cube(4))
 
 print('-----------')
 
 print(calculator(sqare, 5))
 print(calculator(cube, 5))
 print(calculator(print, 5))
+print(calculate(mul, 2, 3))
+print(calculate(add, 3, 4))
+print(calculate(cube, 5))
