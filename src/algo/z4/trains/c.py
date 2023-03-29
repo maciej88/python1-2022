0382c5a656033559644555bfd2a6420cf78bb7c1
maciej88 <mkrywult88@gmail.com>
2023-03-29 18:52:31 +0200

    trains c

--- src/algo/z4/trains/c.py
+++ src/algo/z4/trains/c.py
@@ -4,7 +4,24 @@ zadanie3:
  z miasta "a" do miasta "b" z dokładnie jedną przesiadką, czyli czy istnieje para połączeń typu (a,c),(c,b).
 
 """
+from gen_train_data import generate_data
 
 
-def is_connected_with_stopover(train_data: list[tuple[int, int]], a: int, b: int) -> bool:
-    pass
+def is_connected_with_stopover(train_data: list[tuple[int, int]], a: int, b: int):
+    val = []
+    for i in train_data:
+        if a in i or b in i:
+            val.append(i)
+    print(val)
+    for j, tupla1 in enumerate(val):
+        for tupla2 in val[j + 1:]:
+            if tupla1[1] == tupla2[0] and tupla1[1] != a and tupla1[1] != b and tupla2[0] != a and tupla2[0] != b:
+                if (tupla1[0] == a and tupla2[1] == b) or (tupla1[0] == b and tupla2[1] == a):
+                    return True
+    return False
+
+
+if __name__ == '__main__':
+    rr = generate_data(10)
+    print(rr)
+    print(is_connected_with_stopover(rr, 13, 12))
