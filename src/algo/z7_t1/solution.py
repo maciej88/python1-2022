968668e29cbf1c8c898ba882e29dfa380e20ff7d
PolishTanker <wojciech.borak@gmail.com>
2023-05-13 14:31:19 +0200

    Poprawka do rozwiazania

--- src/algo/z7_t1/solution.py
+++ src/algo/z7_t1/solution.py
@@ -19,7 +19,7 @@ def open_lock(key: list[int], lock: list[int]) -> int:
     :param lock:  np  [1,4,2,2,4,3]
     :return:    -1 jeśli wartości są "invalid" (np. <0); 0 jeśli klucz otwiera zamek, 1 jeśli klucz nie otwiera zamka
     """
-    if len(lock)<0 or len(key)<0 or len(lock)!=len(key) or min(lock)<0 or min(key)<0:
+    if len(lock)<=0 or len(key)<=0 or len(lock)!=len(key) or min(lock)<=0 or min(key)<=0:
         return -1
     war = lock[0] + key[0]
     for i in range(1,len(lock)):
@@ -29,3 +29,4 @@ def open_lock(key: list[int], lock: list[int]) -> int:
 
 key = [4,1,1,3,1,2]
 lock = [1,4,2,2,4,3]
+print(open_lock(lock, key))
